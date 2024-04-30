4d48e04330e8e723198007fa064310a4efe44a77
siffahim <siffahim25@gmail.com>
2024-04-30 12:31:19 +0600

    feat: :fire: banner updated

--- src/app/(group-others)/services/[id]/page.tsx
+++ src/app/(group-others)/services/[id]/page.tsx
@@ -1,13 +1,7 @@
-"use client";
-
 import Contact from "@/components/common/Contact";
 import Media from "@/components/ui/ServiceDetail/Media";
 
-const ServiceDetailsPage = ({ params }: { params: string }) => {
-  // const id = useParams();
-  // const path = usePathname();
-  // const search = useRouter();
-
+const ServiceDetailsPage = () => {
   return (
     <div className="container my-10">
       <Media />
